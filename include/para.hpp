131181e284f29a874f9cc4516960d4b793067ef2
blackbhc <blackbhc@icloud.com>
2024-08-14 17:37:38 +0800

    bak

--- include/para.hpp
+++ include/para.hpp
@@ -1,15 +1,15 @@
 /**
  * @file para.hpp
- * @brief The parameter parser.
+ * @brief The parameter parser and its utils.
  */
 
 #ifndef PARA_HEADER
 #define PARA_HEADER
 #include "../include/toml.hpp"
-#ifdef DEBUG  // to avoid repeated definition or macros, must be called after toml.hpp
-#include "../include/myprompt.hpp"
-#endif
+#include "recenter.hpp"
+#include <cstdint>
 #include <string_view>
+#include <vector>
 
 /**
  * @class runtime_para
@@ -24,5 +24,104 @@ public:
 
 private:
     void read_one_by_one();
+};
+
+/**
+ * @class recenter_para
+ * @brief The parameters used for recenter.
+ *
+ */
+struct recenter_para
+{
+    bool   enable;
+    double radius;
+    double initialGuess[ 3 ];
+
+    // From include/recenter.hpp:
+    // enum class recenter_method : std::uint8_t { COM = 0, MBP = 1 };
+    recenter_method method;
+};
+
+/**
+ * @class align_para
+ * @brief The parameters used for alignment.
+ *
+ */
+struct align_para
+{
+    bool   enable;
+    double radius;
+};
+
+/**
+ * @class image_para
+ * @brief The parameters used for image calculation.
+ *
+ */
+struct image_para
+{
+    bool         enable;
+    double       halfLength;
+    unsigned int binNum;
+};
+
+/**
+ * @class basic_bar_para
+ * @brief The parameters used for bar info calculation, A2, Sbar et al.
+ *
+ */
+struct basic_bar_para
+{
+    bool   enable;
+    double rmin;
+    double rmax;
+};
+
+/**
+ * @class id_random_selection_para
+ * @brief The parameters used for random selection of particle ids.
+ *
+ */
+struct id_random_selection_para
+{
+    bool                        enable;
+    double                      fraction;
+    std::vector< unsigned int > partTypes;
+};
+
+enum class coordinate_frame : std::uint8_t { CYLINDRICAL = 0, SPHERICAL, CARTESIAN };
+
+/**
+ * @class component
+ * @brief The wrapper of parameter blocks used for each component.
+ *
+ */
+class component
+{
+public:
+    component( std::string_view& compName, toml::table& para );
+    auto operator[]( std::string_view& key );
+
+private:
+    std::string_view compName;
+    unsigned int     period;
+    recenter_para    recenterPara;
+    coordinate_frame frame;
+    align_para       alignPara;
+    image_para       imagePara;
+    basic_bar_para   A2Para;
+    basic_bar_para   barAnglePara;
+    basic_bar_para   bucklePara;
+};
+
+/**
+ * @class orbit
+ * @brief The wrapper of parameter blocks used for orbital log.
+ *
+ */
+class orbit
+{
+public:
+    unsigned int period;
 };
 #endif
--- src/para.cpp
+++ src/para.cpp
@@ -1,4 +1,9 @@
 #include "../include/para.hpp"
+#include "../include/myprompt.hpp"
+#include <string>
+#include <string_view>
+using namespace std;
+
 
 runtime_para::runtime_para( const std::string_view& tomlParaFile )
     : paraTable( toml::parse_file( tomlParaFile ) )
@@ -34,4 +39,76 @@ void runtime_para::read_one_by_one()
     // double z      = iguess[ 2 ].value< double >().value();
     // std::cout << "iguess: " << x << ", " << y << ", " << z << "\n";
     ;
+}
+
+component::component( string_view& compName, toml::table& para ) : compName( compName )
+{
+    auto compNode = para[ compName ];
+    // period
+    period = compNode[ "period" ].value< double >().value();
+
+    // recenter parameters
+    recenterPara.enable = compNode[ "recenter" ][ "enable" ].value< bool >().value();
+    recenterPara.radius = compNode[ "recenter" ][ "radius" ].value< bool >().value();
+    auto str            = compNode[ "recenter" ][ "method" ].value< string_view >().value();
+    if ( str == "com" )
+    {
+        recenterPara.method = recenter_method::COM;
+    }
+    else if ( str == "mbp" )
+    {
+        recenterPara.method = recenter_method::MBP;
+    }
+    else
+    {
+        ERROR( "Get an unknown value for [recenter method] of component [%s]", compName.data() );
+        ERROR( "Must be 'com' (for center of mass) or 'mbp' (for most bound particle)." );
+        exit( -1 );
+    }
+    auto iguess = compNode[ "recenter" ][ "iguess" ];
+    for ( auto i = 0; i < 3; ++i )
+        recenterPara.initialGuess[ i ] = iguess[ i ].value< double >().value();
+
+
+    // frame
+    str = compNode[ "frame" ].value< string_view >().value();
+    if ( str == "cyl" )
+    {
+        frame = coordinate_frame::CYLINDRICAL;
+    }
+    else if ( str == "sph" )
+    {
+        frame = coordinate_frame::SPHERICAL;
+    }
+    else if ( str == "car" )
+    {
+        frame = coordinate_frame::CARTESIAN;
+    }
+    else
+    {
+        ERROR( "Get an unknown value for [coordinate frame] of component [%s]", compName.data() );
+        ERROR( "Must be one of 'cyl' (for cylindrical), 'car' (for Cartesian), or 'sph' (for "
+               "spherical)." );
+        exit( -1 );
+    }
+
+    // align
+    alignPara.enable = compNode[ "align" ][ "enable" ].value< bool >().value();
+    alignPara.radius = compNode[ "align" ][ "radius" ].value< double >().value();
+
+    // image
+    imagePara.enable     = compNode[ "image" ][ "enable" ].value< bool >().value();
+    imagePara.halfLength = compNode[ "image" ][ "halflength" ].value< double >().value();
+    imagePara.binNum     = compNode[ "image" ][ "binnum" ].value< unsigned int >().value();
+
+    // bar info parameters
+    A2Para.enable       = compNode[ "A2" ][ "enable" ].value< bool >().value();
+    A2Para.rmin         = compNode[ "A2" ][ "rmin" ].value< double >().value();
+    A2Para.rmax         = compNode[ "A2" ][ "rmax" ].value< double >().value();
+    barAnglePara.enable = compNode[ "barangle" ][ "enable" ].value< bool >().value();
+    barAnglePara.rmin   = compNode[ "barangle" ][ "rmin" ].value< double >().value();
+    barAnglePara.rmax   = compNode[ "barangle" ][ "rmax" ].value< double >().value();
+    bucklePara.enable   = compNode[ "buckle" ][ "enable" ].value< bool >().value();
+    bucklePara.rmin     = compNode[ "buckle" ][ "rmin" ].value< double >().value();
+    bucklePara.rmax     = compNode[ "buckle" ][ "rmax" ].value< double >().value();
 }
